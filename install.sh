6d2a9302fc5f69c0edcb06834d5a522fd23f5588
Dalton Sconce <dscone@protonmail.com>
2019-07-27 20:40:25 -0400

    Fixed the install script

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+CURRENT_DIR=$(pwd)
+
 cd max
 $HOME/.cargo/bin/cargo build --release
-sudo ln -sf ./max/target/release/max /usr/local/sbin/
+sudo ln -sf $CURRENT_DIR/max/target/release/max /usr/local/sbin/
 cd ..
-sudo ln -sf ./update_boot_entry.sh /usr/local/sbin
-sudo ln -sf ./systemd_files/sample_boot_entry /usr/local/sbin
+sudo ln -sf $CURRENT_DIR/update_boot_entry.sh /usr/local/sbin/
+sudo ln -sf $CURRENT_DIR/systemd_files/sample_boot_entry /usr/local/sbin/
 cd systemd_files
 sudo cp $(ls) /etc/systemd/system/
 sudo systemctl enable update_snapshot_entry.timer
