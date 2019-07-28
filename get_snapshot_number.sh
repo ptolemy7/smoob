25e0cb9d3758d06f38a2f52eaa03319ffe5b575c
Dalton Sconce <dscone@protonmail.com>
2019-07-27 20:23:25 -0400

    moved some files around

--- get_snapshot_number.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-LATESTBOOT_LIST=$(/usr/bin/snapper -c root list |
-/usr/bin/grep boot |
- /usr/bin/sort -rg |
- /usr/bin/cut -b -5)
- LATESTBOOT=$(./max $(./get_snapshot_number.sh))
- cp sample_boot_entry /boot/loader/entries/boot_snapshot.conf
- echo "options rd.luks.name=2efb18ce-dfa4-427c-85d9-ba8e70f28b74=crypt rootflags=subvol=arch/.snapshots/"$LATESTBOOT"/snapshot root=/dev/omnia/btrfs" >> /boot/loader/entries/boot_snapshot.conf
