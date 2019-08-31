544e0af65cc34745e0015de734149236de6a67fd
Dalton Sconce <dscone@protonmail.com>
2019-08-31 01:16:37 -0400

    tweaked install file and systemd.timer

--- install.sh
+++ install.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-CURRENT_DIR=$(pwd)
-
-cd max
-$HOME/.cargo/bin/cargo build --release
-sudo ln -sf $CURRENT_DIR/max/target/release/max /usr/local/sbin/
-cd ..
-sudo ln -sf $CURRENT_DIR/update_snapshot_entry.sh /usr/local/sbin/
-cd systemd_files
-sudo cp $(ls) /etc/systemd/system/
+install -D system_files/* /etc/systemd/system/
+install -D update_snapshot_entry.sh /usr/local/sbin/
 sudo systemctl enable update_snapshot_entry.timer
