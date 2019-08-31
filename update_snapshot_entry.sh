49c96a1e20a392beb72349b75efa199e0844f566
Dalton Sconce <dscone@protonmail.com>
2019-08-31 00:58:16 -0400

    got rid of the rust dependency

--- update_snapshot_entry.sh
+++ update_snapshot_entry.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-LATESTBOOT_LIST=$(/usr/bin/snapper -c root list |
+#declare -a latestboot_array
+latestboot_array=($(/usr/bin/snapper -c root list |
 /usr/bin/grep boot |
- /usr/bin/cut -b -5)
- LATESTBOOT=$(/usr/local/sbin/max $LATESTBOOT_LIST)
-echo "title Well Crap . . . (snapshot #"$LATESTBOOT")
+ /usr/bin/cut -b -5 |
+ sort -nr))
+ #LATESTBOOT=${latestboot_array[0]}
+echo "title Well Crap . . . (snapshot #"${latestboot_array[0]}")
 linux /vmlinuz-linux
 initrd /intel-ucode.img
 initrd /initramfs-linux.img
-options rd.luks.name=2efb18ce-dfa4-427c-85d9-ba8e70f28b74=crypt rootflags=subvol=arch/.snapshots/"$LATESTBOOT"/snapshot root=/dev/omnia/btrfs" > /boot/loader/entries/boot_snapshot.conf
+options rd.luks.name=2efb18ce-dfa4-427c-85d9-ba8e70f28b74=crypt rootflags=subvol=arch/.snapshots/"${latestboot_array[0]}"/snapshot root=/dev/omnia/btrfs" > /boot/loader/entries/boot_snapshot.conf
